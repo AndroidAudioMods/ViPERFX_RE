035d5b40255b6c58f2ae701ad0fc597ff8fff580
myself <mail@mail.mail>
2024-12-29 11:49:58 +0000

    make it build on linux

--- src/log.h
+++ src/log.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#ifdef __ANDROID__
+
 #include <android/log.h>
 
 #define TAG "ViPER4Android"
@@ -7,3 +9,13 @@
 #define VIPER_LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define VIPER_LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define VIPER_LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+#else
+
+#include <cstdio>
+
+#define VIPER_LOGD(...) fprintf(stderr, __VA_ARGS__)
+#define VIPER_LOGI(...) fprintf(stderr, __VA_ARGS__)
+#define VIPER_LOGE(...) fprintf(stderr, __VA_ARGS__)
+
+#endif
